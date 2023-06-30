32a146d09411ebeaa05de8dae3a2be2801dc0058
Jordyn <jlewis162030@gmail.com>
2023-06-30 13:13:58 -0500

    3

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     <div className="app">
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
