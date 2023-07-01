7d1aacdfa2a119eca022141492bb8f1a8d548614
Jordyn <jlewis162030@gmail.com>
2023-07-01 11:30:20 -0500

    new commit

--- src/js/pages/About.js
+++ src/js/pages/About.js
@@ -1,6 +1,19 @@
 function About() {
     return (
         <div className="about page-top-padding page-min-height">
+            <div className="about-content">
+                <h1>About</h1>
+                <p>
+                    This website was created to display the hex code color scheme for the lights
+                    on the Empire State Building. The color description that the 
+                    <a href="https://esbnyc.com" target="__">Official Empire State Building</a>
+                    website provides is quite verbose and doesn't actually provide any hex codes.
+                </p>
+                <p>
+                    To address this issue I use the <a href="https://openai.com/blog/openai-api" target="__">OpenAI API</a> to generate a color scheme based on
+                    the color description provided by the Empire State Building website.
+                </p>
+            </div>
         </div>
     )
 }
