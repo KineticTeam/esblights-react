c4e3f71f47d7b7f65cce6d4d90ded04b45e4bccc
Jordyn <jlewis162030@gmail.com>
2023-07-01 12:35:46 -0500

    new commit

--- src/js/pages/Contact.js
+++ src/js/pages/Contact.js
@@ -1,43 +1,22 @@
-import { Form } from "react-router-dom";
-
 function Contact() {
+
+
     return (
         <div className="contact-page page-top-padding page-min-height">
-            <div className="contact-content">
-                <section id="contact">
-    
-                    <h1 class="section-header">Contact</h1>
-                    
-                    <div class="contact-wrapper">
-                        
-                        <form id="contact-form" class="form-horizontal" role="form">
-                        
-                        <div class="form-group">
-                            <div class="col-sm-12">
-                            <input type="text" class="form-control" id="name" placeholder="NAME" name="name" value="" required />
-                            </div>
-                        </div>
+            <div class="middle-content">
+                <div class="contact-header">
+                    <h1>Contact</h1>
+                </div>
+                <div class="contact-body">
+                    <form action={submitForm}>
+                        <input type="text" id="name" name="name" placeholder="Name" required />
+                        <input type="email" id="email" name="email" placeholder="Email Address" required />
+                        <input type="text" id="subject" name="subject" placeholder="Subject" required />
+                        <textarea type="textarea" id="message" name="message" placeholder="Message" required></textarea>
 
-                        <div class="form-group">
-                            <div class="col-sm-12">
-                            <input type="email" class="form-control" id="email" placeholder="EMAIL" name="email" value="" required />
-                            </div>
-                        </div>
-
-                        <textarea class="form-control" rows="10" placeholder="MESSAGE" name="message" required></textarea>
-                        
-                        <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
-                            <div class="alt-send-button">
-                            <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
-                            </div>
-                        </button>
-
-                        </form>
-
-                    </div>
-
-                </section>
-                  
+                        <input type="submit" id="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
         </div>
     )
