3648abd745d702081fcaa1132b77727d34c6f140
Jordyn <jlewis162030@gmail.com>
2023-07-07 13:23:07 -0500

    built

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from 'App';
 import 'styles/global/global.css';
-
-fetch('https://api.ipify.org/?format=json')
-  .then((response) => response.json())
-  .then((data) => {
-    const ipAddress = data.ip;
-    console.log('IP address:', ipAddress);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
