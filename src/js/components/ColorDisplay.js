1ce9314ce6d5a8748d27f7f1d6ae1019c2d74af7
JordynLewi5 <jlewis162030@gmail.com>
2023-07-13 14:56:34 -0500

    new commit

--- src/js/components/ColorDisplay.js
+++ src/js/components/ColorDisplay.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 function ColorDisplay() {
     const [content, setContent] = useState(null);
-
+    
     useEffect(() => {
         const fetchData = async () => {
-            try {
-                const { REACT_APP_ESBAPIKEY } = process.env;
-                const response = await fetch(`https://esblight-api.kinetic.com/api/esb-light-data?apikey=${REACT_APP_ESBAPIKEY}`);
-                const data = await response.json();
-                setContent(data.content);
-            } catch (error) {}
+            const { REACT_APP_ESBAPIKEY } = process.env;
+            const response = await fetch(`https://esblight-api.kinetic.com/api/esb-light-data?apikey=${REACT_APP_ESBAPIKEY}`);
+            const data = await response.json();
+            setContent(data.content);
         };
         fetchData();
     }, []);
@@ -21,25 +19,28 @@ function ColorDisplay() {
 
             <div className="color-section">
                 <div className="color-display">
-                    {content ? (content.hexCodes.map((color, index) => (
-                        <div key={index} className="color-display-card" style={{ background: color }}>
-                            <h2 className="display-hex-code">{color}</h2>
-                        </div>
-                    ))) : (
-                        ""
+                    {content ? (
+                        content.hexCodes.map((color, index) => (
+                            <div
+                                key={index}
+                                className={`color-display-card ${content ? 'fade-in' : ''}`}
+                                style={{ background: color }}
+                            >
+                                <h2 className="display-hex-code">{color}</h2>
+                            </div>
+                        ))
+                    ) : (
+                        ''
                     )}
                 </div>
                 <div className="color-information">
                     <div className="color-description">
-                        <h2>{content ? content.colorDescription : "..."}</h2>
-                        {/* <h3>{content ? "Hex Codes Array:" : ""}</h3>
-                        <code>{content ? JSON.stringify(content.hexCodes) : ""}</code>
-                        <h3>{content ? "XYZ Color Space Array:" : ""}</h3>
-                        <code>{content ? JSON.stringify(content.xyzCodes) : ""}</code> */}
+                        <h2>{content ? content.colorDescription : '...'}</h2>
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
     );
-} 
+}
+
 export default ColorDisplay;
