3f92653b6847b0a7fea21dfb4586e1012d6325fa
JordynLewi5 <jlewis162030@gmail.com>
2023-08-14 11:43:19 -0500

    new commit

--- src/js/components/PastColorDisplay.js
+++ src/js/components/PastColorDisplay.js
@@ -7,7 +7,7 @@ function PastColorDisplay() {
         const fetchData = async () => {
             try {
                 const { REACT_APP_ESBAPIKEY } = process.env;
-                const response = await fetch(`https://esblight-api.kinetic.com/api/past-esb-light-data?apikey=${REACT_APP_ESBAPIKEY}`);
+                const response = await fetch(`http://localhost:4000/api/past-esb-light-data?apikey=${REACT_APP_ESBAPIKEY}`);
                 const data = await response.json();
                 setContent(data.content);
             } catch (error) {
