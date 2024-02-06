c142d7748946cc68d86337fc14e2541f1d51a172
JordynLewi5 <jlewis162030@gmail.com>
2024-02-06 10:16:44 -0600

    new commit

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -61,7 +61,7 @@ function Home() {
                 <div className="api-info">
                     <h2>ESB Lights API</h2>
                     <h3>Request</h3>
-                    <pre className="code-content">GET http://localhost:4000/api/esb-light-data</pre>
+                    <pre className="code-content">GET https://esblight-api.kinetic.com/api/esb-light-data</pre>
                     <h3>Response</h3>
                     <JsonDisplay className="code-content" jsonData={jsonData} />
                     <div><hr /></div>
